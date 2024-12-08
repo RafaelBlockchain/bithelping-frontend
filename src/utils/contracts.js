62ac6136b20bceeab194be7b0e497d15de20e339
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-08 13:38:00 -0500

    Update contracts.js

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -1,1 +1,16 @@
+import { ethers } from "ethers";
+import BITH_ABI from './BITH_ABI.json'; // ABI del contrato BitHelping
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+const bithContract = new ethers.Contract(
+  "0xCONTRACT_ADDRESS", // Dirección del contrato BITH
+  BITH_ABI,
+  signer
+);
+
+export async function getBalance(address) {
+  const balance = await bithContract.balanceOf(address);
+  return ethers.utils.formatUnits(balance, 18); // Convertir a formato legible
+}
 
