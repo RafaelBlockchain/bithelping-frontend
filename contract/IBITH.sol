2dcf7a8bff4937ff58f1a129bf408ccb0b0879bc
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-09 14:58:11 -0500

    Update IBITH.sol

--- contract/IBITH.sol
+++ contract/IBITH.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.0;
 
 interface IBITH {
-    function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
 }
 
