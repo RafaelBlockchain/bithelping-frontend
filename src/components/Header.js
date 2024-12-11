c53a7dbd81400d6a818586a616075b958e49499a
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-10 20:43:24 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,11 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <h1>BitHelping Dashboard</h1>
+    <p>Administra todas las operaciones de tu contrato BitHelpingIntegration.</p>
+  </header>
+);
+
+export default Header;
 
