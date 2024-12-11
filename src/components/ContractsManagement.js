896b6f8a4c1b0b6fe4de4e43a816cac71956da9c
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-10 20:46:11 -0500

    Update ContractsManagement.js

--- src/components/ContractsManagement.js
+++ src/components/ContractsManagement.js
@@ -1,1 +1,21 @@
+import React from "react";
+
+const ContractsManagement = ({ contract }) => {
+  const updateContractAddress = async (contractType, address) => {
+    await contract.methods[`update${contractType}Contract`](address).send({ from: window.ethereum.selectedAddress });
+    alert(`${contractType} actualizado correctamente.`);
+  };
+
+  return (
+    <div>
+      <h3>Administrar Contratos Integrados</h3>
+      <input type="text" placeholder="Nuevo contrato de Staking" id="stakingAddress" />
+      <button onClick={() => updateContractAddress("Staking", document.getElementById("stakingAddress").value)}>
+        Actualizar Staking
+      </button>
+    </div>
+  );
+};
+
+export default ContractsManagement;
 
