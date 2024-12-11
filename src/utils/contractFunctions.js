598ace01a05565a83fdb55985d7eea475168bea3
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-10 20:54:38 -0500

    Update contractFunctions.js

--- src/utils/contractFunctions.js
+++ src/utils/contractFunctions.js
@@ -1,1 +1,19 @@
+import { ethers } from "ethers";
+import { BITH_CONTRACT } from "./contracts";
+
+const getContract = () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  return new ethers.Contract(BITH_CONTRACT.address, BITH_CONTRACT.abi, signer);
+};
+
+export const getBalance = async (address) => {
+  const contract = getContract();
+  return await contract.balanceOf(address);
+};
+
+export const stakeTokens = async (amount) => {
+  const contract = getContract();
+  return await contract.stakeTokens(ethers.utils.parseEther(amount));
+};
 
