6d72f64751135e95b7eb45cb3246876b23ae53a2
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-10 20:45:13 -0500

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,1 +1,34 @@
+import React, { useState, useEffect } from "react";
+import web3 from "../utils/web3";
+import { contractAddress, contractABI } from "../utils/contractABI";
+
+const Dashboard = () => {
+  const [balance, setBalance] = useState(0);
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+
+  useEffect(() => {
+    const loadBlockchainData = async () => {
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+
+      const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+      setContract(contractInstance);
+
+      const balance = await contractInstance.methods.balanceOf(accounts[0]).call();
+      setBalance(web3.utils.fromWei(balance, "ether"));
+    };
+
+    loadBlockchainData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Bienvenido, {account}</h2>
+      <p>Saldo de BITH: {balance} BITH</p>
+    </div>
+  );
+};
+
+export default Dashboard;
 
