603124e25f78d3c3b4c4c4bd23488599e5424388
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 09:01:35 -0500

    Update RealTimeUpdates.js

--- src/components/RealTimeUpdates/RealTimeUpdates.js
+++ src/components/RealTimeUpdates/RealTimeUpdates.js
@@ -1,1 +1,34 @@
+// src/components/RealTimeUpdates/RealTimeUpdates.js
+import React, { useEffect, useState } from 'react';
+
+const RealTimeUpdates = ({ contract, account }) => {
+  const [updates, setUpdates] = useState([]);
+
+  useEffect(() => {
+    const handleNewUpdate = (update) => {
+      setUpdates((prevUpdates) => [...prevUpdates, update]);
+    };
+
+    contract.events
+      .Transfer({ filter: { from: account } })
+      .on('data', (event) => handleNewUpdate(event));
+
+    return () => {
+      contract.events.Transfer.removeListener('data', handleNewUpdate);
+    };
+  }, [contract, account]);
+
+  return (
+    <div className="real-time-updates">
+      <h2>Actualizaciones en Tiempo Real</h2>
+      <ul>
+        {updates.map((update, index) => (
+          <li key={index}>{update.returnValues.value} tokens enviados a {update.returnValues.to}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RealTimeUpdates;
 
