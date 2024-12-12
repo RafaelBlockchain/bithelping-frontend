4a4f99c457d6438f196d3abc51eaf813a0fae974
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 01:18:12 -0500

    Update Analytics.js

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -1,1 +1,14 @@
+import React from "react";
+import RealTimeLineChart from "./charts/RealTimeLineChart";
+
+const Analytics = ({ realTimeData }) => {
+  return (
+    <div>
+      <h1>Análisis de Datos</h1>
+      <RealTimeLineChart data={realTimeData} />
+    </div>
+  );
+};
+
+export default Analytics;
 
