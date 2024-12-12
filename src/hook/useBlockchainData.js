87b2e4fc1e0c33e2a424284f42d8a8170b5b52e7
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 14:20:51 -0500

    Update useBlockchainData.js

--- src/hook/useBlockchainData.js
+++ src/hook/useBlockchainData.js
@@ -1,1 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useBlockchainData = (contract, methodName, params = []) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (contract && methodName) {
+        try {
+          setLoading(true);
+          const result = await contract.methods[methodName](...params).call();
+          setData(result);
+        } catch (err) {
+          setError(err.message);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchData();
+  }, [contract, methodName, params]);
+
+  return { data, loading, error };
+};
+
+export default useBlockchainData;
 
