ec80c500da083e7139bf4c1b2dfd9e0a97061564
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 08:18:31 -0500

    Update NFTContext.js

--- src/contexts/NFTContext.js
+++ src/contexts/NFTContext.js
@@ -1,1 +1,24 @@
+// src/context/NFTContext.js
+import React, { createContext, useState, useContext } from 'react';
+
+// Crear el contexto
+const NFTContext = createContext();
+
+// Crear el proveedor del contexto
+export const NFTProvider = ({ children }) => {
+  // Estado global para los NFTs
+  const [nfts, setNfts] = useState([]);
+
+  // Función para agregar un nuevo NFT
+  const addNFT = (nft) => setNfts([...nfts, nft]);
+
+  return (
+    <NFTContext.Provider value={{ nfts, addNFT }}>
+      {children}
+    </NFTContext.Provider>
+  );
+};
+
+// Custom hook para usar el contexto
+export const useNFTs = () => useContext(NFTContext);
 
