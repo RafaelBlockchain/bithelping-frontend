0a096e9c31c56408fcb92a37fec1938fe535d5f8
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 14:01:32 -0500

    Update and rename blockchain.js to blockchainConfig.js

---
+++ src/config/blockchainConfig.js
@@ -0,0 +1,4 @@
+export const CONTRACT_ADDRESS = "0xYourContractAddress";
+export const NETWORK_ID = 1; // Mainnet (Ethereum)
+export const INFURA_URL = "https://mainnet.infura.io/v3/your-infura-key";
+
