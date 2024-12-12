aa59c0d9cfc4ce0f89db2e2c6a9377b9857d6683
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 01:50:48 -0500

    Update web3Context.js

--- src/contexts/web3Context.js
+++ src/contexts/web3Context.js
@@ -1,1 +1,44 @@
+import React, { createContext, useState } from "react";
+import Web3 from "web3";
+
+export const Web3Context = createContext();
+
+export const Web3Provider = ({ children }) => {
+  const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState("");
+  const [connector, setConnector] = useState(null);
+
+  const connectWalletConnect = async (walletConnect) => {
+    try {
+      await walletConnect.activate();
+      const provider = await walletConnect.getProvider();
+      const web3Instance = new Web3(provider);
+      setWeb3(web3Instance);
+
+      const accounts = await web3Instance.eth.getAccounts();
+      setAccount(accounts[0]);
+      setConnector(walletConnect);
+
+      walletConnect.on("disconnect", () => {
+        setWeb3(null);
+        setAccount("");
+        setConnector(null);
+      });
+    } catch (error) {
+      console.error("Error al conectar WalletConnect:", error);
+    }
+  };
+
+  return (
+    <Web3Context.Provider
+      value={{
+        web3,
+        account,
+        connectWalletConnect,
+      }}
+    >
+      {children}
+    </Web3Context.Provider>
+  );
+};
 
