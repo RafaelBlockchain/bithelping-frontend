39d802b12c81a0d5ff0f726a6dd74109e502f55e
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 09:02:33 -0500

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,1 +1,14 @@
+// src/components/Footer/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 BitHelping - Todos los derechos reservados</p>
+      <a href="/terms">Términos y condiciones</a> | <a href="/privacy">Política de privacidad</a>
+    </footer>
+  );
+};
+
+export default Footer;
 
