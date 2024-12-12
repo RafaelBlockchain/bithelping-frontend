bcb63377cba67d312a1dde87f5433323a383ca5e
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 09:07:03 -0500

    Update NotificationsContext.js

--- src/contexts/NotificationsContext.js
+++ src/contexts/NotificationsContext.js
@@ -1,1 +1,27 @@
+// src/context/NotificationsContext.js
+import React, { createContext, useState, useContext } from 'react';
+
+const NotificationsContext = createContext();
+
+export const NotificationsProvider = ({ children }) => {
+  const [notifications, setNotifications] = useState([]);
+
+  const addNotification = (message) => {
+    setNotifications((prev) => [...prev, message]);
+  };
+
+  const removeNotification = (index) => {
+    setNotifications((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  return (
+    <NotificationsContext.Provider
+      value={{ notifications, addNotification, removeNotification }}
+    >
+      {children}
+    </NotificationsContext.Provider>
+  );
+};
+
+export const useNotifications = () => useContext(NotificationsContext);
 
