7626ca3870cb1007d402852152159725989b4853
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 11:20:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
+import "./styles/global.css"; // Estilos globales
+import "./index.css"; // Estilos específicos del index
 
+// Context Providers
+import { UserProvider } from "./context/UserContext";
+import { Web3Provider } from "./context/Web3Context";
+import { ThemeProvider } from "./context/ThemeContext"; // Suponiendo que usas un ThemeContext
+
+// Routing
+import { BrowserRouter } from "react-router-dom";
+
+// Internacionalización (i18n)
+import "./i18n"; // Archivo de configuración de i18next
+
+// Redux (si es necesario)
+import { Provider as ReduxProvider } from "react-redux";
+import store from "./store"; // Archivo donde configuraste tu store de Redux
+
+// Reporte de métricas de rendimiento
+import reportWebVitals from "./reportWebVitals";
+
+// Modo desarrollo/producción
+if (process.env.NODE_ENV === "development") {
+  console.log("🚀 La aplicación está en modo desarrollo");
+}
+
+// Renderizar la aplicación
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ReduxProvider store={store}>
+        <ThemeProvider>
+          <UserProvider>
+            <Web3Provider>
+              <App />
+            </Web3Provider>
+          </UserProvider>
+        </ThemeProvider>
+      </ReduxProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
+// Métricas de rendimiento (opcional)
+reportWebVitals(console.log);
