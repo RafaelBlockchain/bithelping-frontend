d62f6bd53cc4df0257e69a06a609037bdc957f00
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 09:10:35 -0500

    Update ContractContext.js

--- src/contexts/ContractContext.js
+++ src/contexts/ContractContext.js
@@ -1,1 +1,21 @@
+// src/context/ContractContext.js
+import React, { createContext, useState, useContext } from 'react';
+
+const ContractContext = createContext();
+
+export const ContractProvider = ({ children }) => {
+  const [contract, setContract] = useState(null);
+
+  const setNewContract = (newContract) => {
+    setContract(newContract);
+  };
+
+  return (
+    <ContractContext.Provider value={{ contract, setNewContract }}>
+      {children}
+    </ContractContext.Provider>
+  );
+};
+
+export const useContract = () => useContext(ContractContext);
 
