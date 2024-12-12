0e08c59f9055cfe7959e241eb4a7b5512bb03c34
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 10:41:30 -0500

    Update validationUtils.js

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -1,5 +1,21 @@
 // src/utils/validationUtils.js
-export const isValidAddress = (address) => {
-  return Web3.utils.isAddress(address);
+
+// Validar que un valor es un número positivo
+export const isPositiveNumber = (value) => {
+  return !isNaN(value) && Number(value) > 0;
 };
 
+// Validar que un string tiene formato de dirección Ethereum
+export const isEthereumAddress = (address) => {
+  return /^0x[a-fA-F0-9]{40}$/.test(address);
+};
+
+// Validar que un hash es válido
+export const isValidHash = (hash) => {
+  return /^0x[a-fA-F0-9]{64}$/.test(hash);
+};
+
+// Validar que un campo no esté vacío
+export const isNotEmpty = (value) => {
+  return value.trim().length > 0;
+};
