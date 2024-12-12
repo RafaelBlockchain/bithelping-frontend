18a8687aecc5b7c75c76ab2c736900c32ee0cfae
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 08:47:14 -0500

    Update web3Utils.js

--- src/utils/web3Utils.js
+++ src/utils/web3Utils.js
@@ -1,1 +1,9 @@
+// src/utils/web3Utils.js
+export const toWei = (amount) => {
+  return Web3.utils.toWei(amount.toString(), "ether");
+};
+
+export const fromWei = (amount) => {
+  return Web3.utils.fromWei(amount.toString(), "ether");
+};
 
