dc5707461a63d83f112abd1a3ecb2b352cd727b0
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 09:11:58 -0500

    Update SettingsContext.js

--- src/contexts/SettingsContext.js
+++ src/contexts/SettingsContext.js
@@ -1,1 +1,21 @@
+// src/context/SettingsContext.js
+import React, { createContext, useState, useContext } from 'react';
+
+const SettingsContext = createContext();
+
+export const SettingsProvider = ({ children }) => {
+  const [settings, setSettings] = useState({ language: 'es', currency: 'BITH' });
+
+  const updateSettings = (newSettings) => {
+    setSettings((prevSettings) => ({ ...prevSettings, ...newSettings }));
+  };
+
+  return (
+    <SettingsContext.Provider value={{ settings, updateSettings }}>
+      {children}
+    </SettingsContext.Provider>
+  );
+};
+
+export const useSettings = () => useContext(SettingsContext);
 
