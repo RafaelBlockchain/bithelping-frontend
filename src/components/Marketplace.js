e22220f6db4de1b6304c3118eb95667dbacfb453
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 07:59:18 -0500

    Update Marketplace.js

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,1 +1,34 @@
+import React, { useState, useEffect } from "react";
+
+const Marketplace = ({ contract }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const itemList = await contract.methods.getListedItems().call();
+      setItems(itemList);
+    };
+    fetchItems();
+  }, [contract]);
+
+  const buyItem = async (itemId) => {
+    await contract.methods.buyItem(itemId).send({ from: account });
+  };
+
+  return (
+    <div>
+      <h2>Marketplace</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.price} ETH
+            <button onClick={() => buyItem(item.id)}>Comprar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Marketplace;
 
