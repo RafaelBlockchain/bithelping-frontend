de440cb7de96cadff991d75540bdf567623b3b3d
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 08:31:21 -0500

    Update walletConnect.js

--- src/utils/walletConnect.js
+++ src/utils/walletConnect.js
@@ -1,12 +1,1 @@
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-
-// Configuración del conector de WalletConnect
-export const walletConnect = new WalletConnectConnector({
-  rpc: {
-    1: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID", // Reemplaza con tu proyecto Infura o un RPC compatible
-    137: "https://polygon-rpc.com", // RPC para Polygon
-  },
-  bridge: "https://bridge.walletconnect.org",
-  qrcode: true,
-});
 
