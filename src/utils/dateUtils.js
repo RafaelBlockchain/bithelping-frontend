965009644a7f9f3feec0be20e7d3418321d787ca
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 08:48:06 -0500

    Update dateUtils.js

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,1 +1,6 @@
+// src/utils/dateUtils.js
+export const formatDate = (date) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(date).toLocaleDateString(undefined, options);
+};
 
