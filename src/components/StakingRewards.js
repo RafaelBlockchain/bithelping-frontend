5099fc33fae61b1ed4e0b50a199968906329f5c0
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 07:58:22 -0500

    Update StakingRewards.js

--- src/components/StakingRewards.js
+++ src/components/StakingRewards.js
@@ -1,1 +1,30 @@
+import React, { useState } from "react";
+
+const StakingRewards = ({ contract }) => {
+  const [amount, setAmount] = useState("");
+
+  const stakeTokens = async () => {
+    await contract.methods.stake(amount).send({ from: account });
+  };
+
+  const claimRewards = async () => {
+    await contract.methods.claimRewards().send({ from: account });
+  };
+
+  return (
+    <div>
+      <h2>Staking de Recompensas</h2>
+      <input
+        type="number"
+        placeholder="Cantidad"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <button onClick={stakeTokens}>Hacer Stake</button>
+      <button onClick={claimRewards}>Reclamar Recompensas</button>
+    </div>
+  );
+};
+
+export default StakingRewards;
 
