cec5f41f2dfd1b1b2e48c37c661ce30b3c126ad1
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 09:09:07 -0500

    Update TransactionContext.js

--- src/contexts/TransactionContext.js
+++ src/contexts/TransactionContext.js
@@ -1,1 +1,21 @@
+// src/context/TransactionContext.js
+import React, { createContext, useState, useContext } from 'react';
+
+const TransactionContext = createContext();
+
+export const TransactionProvider = ({ children }) => {
+  const [transactions, setTransactions] = useState([]);
+
+  const addTransaction = (transaction) => {
+    setTransactions((prev) => [...prev, transaction]);
+  };
+
+  return (
+    <TransactionContext.Provider value={{ transactions, addTransaction }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+};
+
+export const useTransactions = () => useContext(TransactionContext);
 
