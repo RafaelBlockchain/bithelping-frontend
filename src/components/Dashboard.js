a0307b62a8c05924cebe49122db6de4ee876699b
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 01:16:39 -0500

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,7 @@ import web3 from "../utils/web3";
 import { contractAddress, contractABI } from "../utils/contractABI";
 import RealTimeLineChart from "./charts/RealTimeLineChart";
 
-const Dashboard = () => {
+const Dashboard = ({ account, balance, contract, realTimeData }) => {
   const [balance, setBalance] = useState(0);
   const [account, setAccount] = useState("");
   const [contract, setContract] = useState(null);
@@ -27,14 +27,10 @@ const Dashboard = () => {
     loadBlockchainData();
   }, []);
 
+  
   return (
     <div>
       <h2>Bienvenido al Dashboard</h2>
-      <p>Saldo de BITH: {balance} BITH</p>
-      <p>Cuenta conectada: {account}</p>
-      <p>Balance: {balance} BITH</p>
-
-       {/* Sección de gráficos */}
       <h2>Gráficos en Tiempo Real</h2>
       <RealTimeLineChart data={realTimeData} />
     </div>
