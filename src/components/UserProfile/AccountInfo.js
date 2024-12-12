14b9cfde5b64a20203db09eb42c47fdfbd5420f6
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2024-12-12 08:53:56 -0500

    Update AccountInfo.js

--- src/components/UserProfile/AccountInfo.js
+++ src/components/UserProfile/AccountInfo.js
@@ -1,1 +1,18 @@
+// src/components/UserProfile/AccountInfo.js
+import React, { useContext } from 'react';
+import { Web3Context } from '../../contexts/web3Context';
+
+const AccountInfo = () => {
+  const { account, balance } = useContext(Web3Context);
+
+  return (
+    <div className="account-info">
+      <h2>Mi Cuenta</h2>
+      <p>Dirección de Wallet: {account}</p>
+      <p>Balance: {balance} BITH</p>
+    </div>
+  );
+};
+
+export default AccountInfo;
 
